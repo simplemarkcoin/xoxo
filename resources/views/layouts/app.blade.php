8130fdad9d860b001b8ffd298d5118b192d5f2dc
simplemarkcoin <actionszam@gmail.com>
2025-09-03 08:43:56 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -119,3 +119,29 @@
 </body>
 
 <!-- Smartsupp Live Chat script -->
+@verbatim
+<!-- Live Chat -->
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    </head>
+<body>
+    <main>
+        @yield('content')
+    </main>
+
+    @verbatim
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/68a7d2f16beb8c1922ef1431/1j46q6ri2';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    @endverbatim
+</body>
+</html>
